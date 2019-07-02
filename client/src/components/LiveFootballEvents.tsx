e186269d1f270c79cad5a0ff5b6c318bc3743b61
Mihaita Ivli <mihaitaivli@gmail.com>
2019-07-02 23:10:39 +0100

    create skeleton page for football events

--- client/src/components/LiveFootballEvents.tsx
+++ client/src/components/LiveFootballEvents.tsx
@@ -1,8 +1,7 @@
 import React, { Component, Fragment } from 'react'
-import { IEvent } from '../models/Event';
+import { IEvent } from '../models/Event'
 
-
-class LiveFootballEvents extends Component<{}, {data: IEvent | null}> {
+class LiveFootballEvents extends Component<{}, {data: any}> {
     constructor(props: any) {
         super(props)
         this.state = {
@@ -12,20 +11,28 @@ class LiveFootballEvents extends Component<{}, {data: IEvent | null}> {
     componentDidMount() {
         const w = new WebSocket('ws://localhost:8889')
         w.onmessage = (e:MessageEvent) => {
-                this.setState({
-                    data: e.data
-                })}
+            this.setState({
+                data: JSON.parse(e.data)
+            })}
         w.onopen = () => w.send(JSON.stringify({ type: "getLiveEvents" }));
     }
-
+        
     render() {
         const  { data } = this.state
+        if(data) {
+            console.log(data.data)
+        }
         return (
             <Fragment>
-                {data && <div>{data}</div>}
+                {data && data.type === 'LIVE_EVENTS_DATA' && data.data.map((event: IEvent) => (
+                    <div key={event.eventId}>
+                        {event.name}
+                    </div>
+                ))}
             </Fragment>
         )
     }
+    
 }
 
 export default LiveFootballEvents
