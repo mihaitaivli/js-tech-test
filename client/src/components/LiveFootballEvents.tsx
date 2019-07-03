bf5f7656e593d3bb1e8c1b3eba50f9543d03fb78
Mihaita Ivli <mihaitaivli@gmail.com>
2019-07-03 07:10:20 +0100

    close on unmount

--- client/src/components/LiveFootballEvents.tsx
+++ client/src/components/LiveFootballEvents.tsx
@@ -2,21 +2,27 @@ import React, { Component, Fragment } from 'react'
 import { IEvent } from '../models/Event'
 
 class LiveFootballEvents extends Component<{}, {data: any}> {
+    private w: WebSocket
     constructor(props: any) {
+        
         super(props)
+        this.w = new WebSocket('ws://localhost:8889')
         this.state = {
             data: null
         }
     }
     componentDidMount() {
-        const w = new WebSocket('ws://localhost:8889')
-        w.onmessage = (e:MessageEvent) => {
+        // const w = new WebSocket('ws://localhost:8889')
+        this.w.onmessage = (e:MessageEvent) => {
             this.setState({
                 data: JSON.parse(e.data)
             })}
-        w.onopen = () => w.send(JSON.stringify({ type: "getLiveEvents" }));
+        this.w.onopen = () => this.w.send(JSON.stringify({ type: "getLiveEvents" }));
     }
-        
+    componentWillUnmount() {
+        this.w.close()
+    }
+
     render() {
         const  { data } = this.state
         if(data) {
